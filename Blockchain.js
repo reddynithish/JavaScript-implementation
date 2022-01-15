e4aaaf2348c56fddbf706fecfec8af885105bd6d
Nithish Reddy S <56553863+reddynithish@users.noreply.github.com>
2022-01-15 16:18:40 +0530

    My new poject

---
+++ Blockchain.js
@@ -0,0 +1,23 @@
+const Block = require('./block.js');
+
+class Blockchain
+{
+    constructor()
+    {
+        this.chain = [Block.genesis()];
+    }
+
+    addBlock({ data })
+    {
+        const newBlock = Block.mineBlock(
+            {
+                lastBlock: this.chain[this.chain.length-1],
+                data
+            }
+        );
+        this.chain.push(newBlock);
+    }
+
+}
+
+module.exports = Blockchain;
---
+++ genesis.js
@@ -0,0 +1,9 @@
+const genesis_info ={
+    timestamp: new Date(),
+    previousHash:' 4483babe4c6188a8a0d0226b01c8aeae6cf07b77bc73bc98b7e2eda0f3b09dee',
+    currentHash: '9cc2d831a2f956a93378156057a41d41a804923eb38e583f414a9cc18dc3e8af',
+    data: 'Block Number: 0'
+};
+
+
+module.exports = {genesis_info};
